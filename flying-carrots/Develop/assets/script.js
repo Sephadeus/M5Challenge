278904973445012c9d323c13346b6a6cb98ee1f7
Sephadeus <90430093+Sephadeus@users.noreply.github.com>
2022-10-18 22:17:57 -0400

    fixed js, local storage issues

--- flying-carrots/Develop/assets/script.js
+++ flying-carrots/Develop/assets/script.js
@@ -1,125 +1,114 @@
 var containerEl = document.querySelector(".container");
-
+var text = $(this).siblings(".textarea").val();
+var time = $(this).siblings(".textarea").attr("data-number");
 
 function update() {
-    $('#currentDay').html(moment().format('MMMM Do YYYY, h:mm:ss a'));
+  $("#currentDay").html(moment().format("MMMM Do YYYY, h:mm:ss a"));
 }
 setInterval(update, 1000);
 
-
-
-
-var containerElement = $('div');
-    containerElement.css('width', '100%')
-    containerElement.addClass('description')
+var containerElement = $("div");
+containerElement.css("width", "100%");
+containerElement.addClass("description");
 
 //Array for times
 var times = [9, 10, 11, 12, 1, 2, 3, 4, 5];
 //Array for military time values
 var milTimes = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-console.log(times)
-
-
-
+//console.log(times)
+console.log(time)
+var savedTasks = localStorage.getItem(time);
+console.log(savedTasks);
 //  Add a div for each work day hour
 for (var i = 0; i < times.length; i++) {
+  // Time Block Div
+  var timeBlockElement = $("<div>");
+  timeBlockElement.addClass("row time-block");
 
-    var savedTasks = JSON.parse(localStorage.getItem("block"));
-    console.log(savedTasks);
-        // Time Block Div
-    var timeBlockElement = $('<div>');
-    timeBlockElement.addClass('row time-block');
+  console.log(timeBlockElement);
+  // Hour
+  var timeHour = $("<p>");
+  timeHour.addClass("hour");
 
-    console.log(timeBlockElement)
-        // Hour
-    var timeHour = $('<p>');
-        timeHour.addClass('hour')
+  if (milTimes[i] >= 9 && milTimes[i] < 12) {
+    timeHour.text(times[i] + " AM");
+  } else {
+    timeHour.text(times[i] + " PM");
+  }
+  // Event
+  var inputSchedule = $("<input>");
+  inputSchedule.attr("text");
+  inputSchedule.addClass("textarea input");
+  inputSchedule.attr("data-number", milTimes[i]);
+  inputSchedule.css("width", "70%");
+  inputSchedule.val(localStorage.getItem(times[i]));
 
-        if (milTimes[i] >= 9 && milTimes[i] < 12){
-        timeHour.text(times[i] + " AM")
-        } else {
-            timeHour.text(times[i] + " PM")
-        }
-        // Event
-    var inputSchedule = $('<input>');
-        inputSchedule.attr('text')
-        inputSchedule.addClass('textarea input');
-        inputSchedule.attr('data-number', milTimes[i])
-        inputSchedule.css('width', '70%');   
-        
-    var listTask = $('<li>');
-        listTask.addClass('listTask');
-        listTask.attr('data-number', milTimes[i]);
-        listTask.text(savedTasks[i].taskName);
-       
-    var myTasks = [];
-        // Save Button
-    var saveButton = $('<button>');
-        saveButton.addClass('saveBtn saveBtn i:hover textarea');
-        saveButton.text('Save')
 
-        saveButton.on("click", function(event) {
-            event.preventDefault();
+  var listTask = $("<li>");
+  listTask.addClass("listTask");
+  listTask.attr("data-number", milTimes[i]);
+  listTask.text(savedTasks);
 
-            var text = $(this).siblings('.textarea').val();
-            
-            var time = $(this).siblings('.textarea').attr("data-number");
+  var myTasks = [];
+  // Save Button
+  var saveButton = $("<button>");
+  saveButton.addClass("saveBtn saveBtn i:hover textarea");
+  saveButton.text("Save");
 
-            var block = {
+  saveButton.on("click", function (event) {
+    event.preventDefault();
+    var text = $(this).siblings(".textarea").val();
+    var time = $(this).siblings(".textarea").attr("data-number");
+
+    console.log(time);
+    console.log(text);
+
+    /*var block = {
                 timeAt: time,
                 taskName: text
             }
+            */
 
+    localStorage.setItem(time, text);
+  });
 
-            console.log(block);
-            myTasks.push(block);
-            console.log(myTasks);
-           
-            localStorage.setItem("block", JSON.stringify(myTasks));
-        });
-       
-        var savedTasks = JSON.parse(localStorage.getItem("block"));
-        console.log(savedTasks);
-    
-        // Appending
-    containerElement.append(timeBlockElement)
-    timeBlockElement.append(timeHour)
-    timeBlockElement.append(inputSchedule)
-    timeBlockElement.append(saveButton)
+  // console.log(savedTasks);
+
+  // Appending
+  containerElement.append(timeBlockElement);
+  timeBlockElement.append(timeHour);
+  timeBlockElement.append(inputSchedule);
+  timeBlockElement.append(saveButton);
 }
 
-
 function timeTracker() {
-    
-    var currentHour = moment().hour();
+  var currentHour = moment().hour();
 
-    console.log(currentHour);
-        
-        console.log($(".row"));
+  //console.log(currentHour);
 
-    $(".textarea").each(function() {
-        
-        var hourTime = $(this).attr("data-number");
-        // Add class based on current time
-        
-        if (hourTime < currentHour ) {
-            $(this).removeClass("future");
-            $(this).removeClass("present");
-            $(this).addClass("past");
-            console.log(hourTime);
+  //console.log($(".row"));
 
-        } else if (hourTime == currentHour) {
-            $(this).removeClass("future");
-            $(this).removeClass("past");
-            $(this).addClass("present");
-            console.log(hourTime);
-        } else if (hourTime > currentHour) {
-            $(this).removeClass("past");
-            $(this).removeClass("present");
-            $(this).addClass("future");
-            console.log(hourTime);
-        }
-    });
+  $(".textarea").each(function () {
+    var hourTime = $(this).attr("data-number");
+    // Add class based on current time
+
+    if (hourTime < currentHour) {
+      $(this).removeClass("future");
+      $(this).removeClass("present");
+      $(this).addClass("past");
+      //console.log(hourTime);
+    } else if (hourTime == currentHour) {
+      $(this).removeClass("future");
+      $(this).removeClass("past");
+      $(this).addClass("present");
+      //console.log(hourTime);
+    } else if (hourTime > currentHour) {
+      $(this).removeClass("past");
+      $(this).removeClass("present");
+      $(this).addClass("future");
+      //console.log(hourTime);
+    }
+  });
 }
 
 timeTracker();
